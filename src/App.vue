34b7dbdd669a8922dc17602e49a4889725cd4188
Jamila <jamilaaliyeva32@gmail.com>
2024-11-15 21:03:30 +0500

    sdf

---
+++ src/App.vue
@@ -0,0 +1,374 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="container">
+    <header>
+   <nav class="navbar">
+   
+   <ul class="header-list">
+      <a href="#"><img src="./assets/image/logo.png" alt=""></a>
+      <li><a href="#">Kurslarimiz</a></li>
+      <li><a href="#">Bog’lanish</a></li>
+   </ul>
+   <div class="header-right">
+    <a href="#"><img src="./assets/image/phone.png"></img></a>
+   <p>(78) 777-47-47</p>
+   </div>
+   </nav>
+  </header>
+  </div>
+  <main>
+    <section class="hero">
+      <div class="container">
+        <div class="hero-wrapper">
+        <div class="hero-left">
+        <h2>PDP Junior – IT kelajakni shu yerdan boshlang!</h2>
+         <div class="left-list">
+         <h3>1</h3>
+         <div class="left-content">
+         <h4>Sifatli Ta’lim</h4>
+         <p>5 yildan beri katta-yu kichik dasturchilarni yetishtirib chiqarmoqda</p>
+         </div>
+         </div>
+
+         <div class="left-list">
+         <h3>2</h3>
+         <div class="left-content">
+         <h4>Bilim va ko’nikma</h4>
+         <p>Yoshlar dasturlash va tanqidiy fikrlash ko'nikmalariga ega bo'lishmoqda</p>
+         </div>
+         </div>
+         <div class="left-list">
+         <h3>1</h3>
+         <div class="left-content">
+         <h4>Sifatli Ta’lim</h4>
+         <p>5 yildan beri katta-yu kichik dasturchilarni yetishtirib chiqarmoqda</p>
+         </div>
+         </div>
+        </div>
+        <div class="hero-right">
+    <div class="center-image">
+        <img src="./assets/image/pdp-logo.png" alt="">
+    </div>
+    <div class="circle-image" style="--angle: 0deg;">
+        <img src="./assets/image/hero-1.png" alt="">
+    </div>
+    <div class="circle-image" style="--angle: 45deg;">
+        <img src="./assets/image/hero-2.png" alt="">
+    </div>
+    <div class="circle-image" style="--angle: 90deg;">
+        <img src="./assets/image/hero-3.png" alt="">
+    </div>
+    <div class="circle-image" style="--angle: 135deg;">
+        <img src="./assets/image/hero-4.png" alt="">
+    </div>
+    <div class="circle-image" style="--angle: 180deg;">
+        <img src="./assets/image/hero-5.png" alt="">
+    </div>
+    <div class="circle-image" style="--angle: 225deg;">
+        <img src="./assets/image/hero-6.png" alt="">
+    </div>
+    <div class="circle-image" style="--angle: 270deg;">
+        <img src="./assets/image/hero-7.png" alt="">
+    </div>
+    <!-- <div class="circle-image" style="--angle: 315deg;">
+        <img src="./assets/image/hero-8.png" alt="">
+    </div> -->
+</div>
+
+        </div>
+      </div>
+    
+    </section>
+
+    <section class="footer">
+    <div class="container">
+     <h2 >Bizning kurslarimiz kimga to’g’ri keladi ?</h2>
+    </div>
+    </section>
+
+
+<section class="lessons">
+<div class="container">
+<div class="lessons-wrapper">
+  <div class="lessons-left">
+<img src="./assets/image/second-page.png" alt="">
+</div>
+<div class="lessons-right">
+<div class="lessons-top">
+  <h3>Asosan 8 -17 yoshdagi bolalar uchun</h3>
+  <p>Bolarda dasturlash yo’nalishiga birinchi qadam tashlash, kompyuter savodxonligini oshirish, mantiqiy fikrlashni kengaytirish, matematik bilimlarni takomillashtirish.</p>
+</div>
+<div>
+<h3>Farzandiga befarq boʻlmagan ota-onalar</h3>
+<ul class="lessons-list">
+     <li class="lessons-item">Farzandining til oʻrganishini xohlaydigan va bunga sharoit yaratib beradigan.</li>
+     <li class="lessons-item">Kelajakda farzandining katta yutuqlaridan faxrlanib yurishni istagan.</li>
+     <li class="lessons-item">Farzandining kelajagi uchun hozirdan bilimga investitsiya qiladigan ota-onalar uchun moʻljallangan.</li>
+</ul>
+</div>
+</div>
+</div>
+</div>
+</section>
+<section class="footer">
+    <div class="container">
+     <h2 >Bizning kurslarimiz kimga to’g’ri keladi ?</h2>
+    </div>
+    </section>
+
+    <footer>
+       <div class="wrapper">
+       <div>
+        <img src="./assets/image/logo.png" alt="">
+       </div>
+       <div class="social">
+        <a href="#"> <img src="./assets/image/telegram.svg" alt=""></a>
+        <a href="#"><img src="./assets/image/instagram.svg" alt=""></a>
+        <a href="#"> <img src="./assets/image/youtube.svg" alt=""></a>
+       </div>
+       <a href="#"><h4>(78) 777-47-47</h4></a>
+     
+       </div>
+       <hr>
+       <div class="info">
+       <p>©2022 PDP Academy</p>
+       <p>Xizmatlar litsenziyalangan</p>
+       </div>
+    </footer>
+  </main>
+
+</template>
+
+<style scoped lang="scss">
+header{
+  background: linear-gradient(to right, #0d1d25, #3aa76d);
+  padding: 16px 0;
+
+  .navbar{
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    max-width: 1200px;
+    margin: 0 auto;
+    padding: 0 16px;
+    .header-list{
+      display: flex;
+      align-items: center;
+      gap: 32px;
+      a{
+        color: rgb(116, 228, 129);
+        font-size: 1.125rem;
+        font-weight: 600;
+        transition: 0.5s;
+        &:hover {
+          color: #bfeed5; 
+        }
+      }
+    }
+    .header-right{
+      display: flex;
+      gap: 15px;
+      align-items: center;
+      p{
+        color: #fff;
+      }
+    }
+
+}
+}
+
+.hero-wrapper{
+      // text-align: center;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        gap: 100px;
+        margin: 100px auto;
+  .hero-left{
+    margin-top: 30px;
+    position: relative;
+    border: 1px solid #8a8787;
+    border-radius: 21px;
+    padding: 21px;
+    width: 483px;
+    background-color: #ffffff; 
+    box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.1);
+    &::before {
+        content: "";
+        position: absolute;
+        top: 10px;
+        left: 10px;
+        // bottom: -15px;
+        width: 100%;
+        height: 100%;
+        background-color: rgba(219, 249, 216, 1); 
+        border-radius: 26px; 
+        z-index: -1; 
+        transform: scale(1.05); 
+    }
+    h2{
+      // font-weight: 800;
+      font-size: 31px;
+      // width: 521px;
+    }
+    .left-list{
+      display: flex;
+      gap: 15px;
+      margin: 15px 0;
+
+      h3{
+        width: 45px;
+        height: 45px;
+        background-color: rgba(219, 249, 216, 1);
+        border-radius: 50%;
+        text-align: center;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+      }
+      .left-content{
+        h4{
+          font-size: 21px;
+          color:  rgba(0, 181, 52, 1);
+          margin-bottom: 8px;
+        }
+        p{
+          color: rgba(1, 42, 48, 1);
+          font-size: 17px;
+          width: 333px;
+        }
+      }
+    }
+  }
+  .hero-right {
+    position: relative;
+    width: 300px;
+    height: 300px;
+    // margin: auto;
+}
+
+.center-image {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-30%, -30%);
+    width: 200px;
+    height: 200px;
+    border-radius: 50%;
+    overflow: hidden;
+}
+
+.center-image img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+}
+
+.circle-image {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: rotate(var(--angle)) translate(150px) rotate(calc(-1 * var(--angle)));
+    width: 90px;
+    height: 90px;
+    border-radius: 50%;
+    overflow: hidden;
+}
+
+.circle-image img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+}
+
+}
+.footer{
+  background-color: rgba(0, 181, 52, 1);
+  padding: 20px;
+  color: #fff;
+  text-align: center;
+  h2{
+
+  }
+}
+footer{
+  background-color: rgba(1, 42, 48, 1);
+  .wrapper{
+    padding: 25px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    .social{
+      display: flex;
+      gap: 15px;
+    }
+    h4{
+      color: rgba(219, 249, 216, 1);
+    }
+  }
+}
+.info{
+  display: flex;
+  justify-content: space-between;
+  color: rgba(219, 249, 216, 1);
+  padding: 10px;
+}
+
+.lessons{
+  .lessons-wrapper{
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        width: 100%;
+    .lessons-left{
+      img{
+        position: relative;
+        width: 600px;
+        margin-left: 200px;
+      }
+    }
+    .lessons-right{
+      display: flex;
+    flex-direction: column;
+    justify-content: center; 
+    height: 100%; 
+    position: absolute;
+    right: 220px;
+    margin: 30px 0;
+    position: relative;
+    border: 1px solid #8a8787;
+    border-radius: 21px;
+    padding: 41px;
+    width: 783px;
+    height: 376px;
+    background-color: #ffffff; 
+    box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.1);
+    &::before {
+        content: "";
+        position: absolute;
+        top: 10px;
+        left: 10px;
+        width: 100%;
+        height: 100%; 
+        background-color: rgba(219, 249, 216, 1); 
+        border-radius: 26px; 
+        z-index: -1; 
+        transform: scale(1.05); 
+    }
+    .lessons-top{
+      margin-bottom: 45px;
+    }
+    h3{
+        padding-bottom: 10px;
+      }
+      .lessons-item{
+        list-style:disc;
+      }
+    }
+  }
+}
+
+</style>
